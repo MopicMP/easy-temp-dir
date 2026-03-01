e5245503d0d16844563ae687ed8f3916f5fcccf9
RaccoNet <zavolokinyegor@gmail.com>
2026-03-01 14:24:04 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easy-temp-dir",
+    version="1.0.0",
+    description="Easy context manager for temp directories",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="raccoonette",
+    url="https://github.com/MopicMP/easy-temp-dir",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
